690fd34404a478ba11e52f851609fef175de1057
Arogundade Ibrahim <devarogundade@gmail.com>
2022-12-04 08:34:07 +0100

    clean up

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -715,51 +715,6 @@ contract Swap {
         );
     }
 
-    // called by any provider
-    function testnetHelper2() public {
-        createPool(
-            0xfcd9eB765936D608C6f9a4cC3C9080190e3A2566,
-            0xac2003580BD6329D1CbE5c492D523fC5b8004d56
-        ); // WBTC => DAI : 1
-        createPool(
-            0xfcd9eB765936D608C6f9a4cC3C9080190e3A2566,
-            0x5b9228d432B21e5Faf6c07792149bEaa0A650032
-        ); // WBTC => WETH : 2
-        createPool(
-            0xfcd9eB765936D608C6f9a4cC3C9080190e3A2566,
-            0x87714Bba938f16C852E91847c0bC95DA3609A2EA
-        ); // WBTC => SAND : 3
-        createPool(
-            0xfcd9eB765936D608C6f9a4cC3C9080190e3A2566,
-            0x934253c5B9455F4694F49163E4D132f3de5A5666
-        ); // WBTC => USDC : 4
-        createPool(
-            0xfcd9eB765936D608C6f9a4cC3C9080190e3A2566,
-            0xa90058c81Ce72946526acb37c444a1Cd640CCC21
-        ); // WBTC => USDT : 5
-        createPool(
-            0x5b9228d432B21e5Faf6c07792149bEaa0A650032,
-            0xa90058c81Ce72946526acb37c444a1Cd640CCC21
-        ); // WETH => USDT : 6
-        createPool(
-            0x5b9228d432B21e5Faf6c07792149bEaa0A650032,
-            0x87714Bba938f16C852E91847c0bC95DA3609A2EA
-        ); // WETH => SAND : 7
-        createPool(
-            0x87714Bba938f16C852E91847c0bC95DA3609A2EA,
-            0xa90058c81Ce72946526acb37c444a1Cd640CCC21
-        ); // SAND => USDT : 8
-        createPool(
-            0x87714Bba938f16C852E91847c0bC95DA3609A2EA,
-            0xac2003580BD6329D1CbE5c492D523fC5b8004d56
-        ); // SAND => DAI : 9
-        createPool(
-            0xac2003580BD6329D1CbE5c492D523fC5b8004d56,
-            0x5b9228d432B21e5Faf6c07792149bEaa0A650032
-        ); // DAI => WETH : 10
-        createPool(NATIVE_PAIR, 0xa90058c81Ce72946526acb37c444a1Cd640CCC21); // MATIC => USDT : 11
-    }
-
     // === Modifiers === //
 
     modifier onlyGuest() {
