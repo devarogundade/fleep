c73ed2deb7572ab4dd810c65ee2b26c04e85fc16
Arogundade Ibrahim <devarogundade@gmail.com>
2022-12-04 11:56:54 +0100

    clean ups

--- migrations/1_deploy_core.js
+++ migrations/1_deploy_core.js
@@ -5,6 +5,6 @@ const Sweeper = artifacts.require("Sweeper");
 
 module.exports = async function(deployer) {
     await deployer.deploy(PriceApi);
-    // await deployer.deploy(Swap, PriceApi.address);
-    // await deployer.deploy(Sweeper, Swap.address);
+    await deployer.deploy(Swap, PriceApi.address);
+    await deployer.deploy(Sweeper, "0x94cD5e6f03d765F3234c3ece3e0Ad22b40e62E39");
 };
--- migrations/2_deploy_testtokens.js
+++ migrations/2_deploy_testtokens.js
@@ -7,10 +7,10 @@ const USDC = artifacts.require("TestnetToken");
 const USDT = artifacts.require("TestnetToken");
 
 module.exports = async function(deployer) {
-    // await deployer.deploy(WBTC, "Bitcoin", "WBTC");
-    // await deployer.deploy(DAI, "DAI", "DAI");
-    // await deployer.deploy(WETH, "Wrapper Ethereum", "WETH");
-    // await deployer.deploy(SAND, "Sandbox", "SAND");
-    // await deployer.deploy(USDC, "USDC", "USDC");
-    // await deployer.deploy(USDT, "USDT", "USDT");
+    await deployer.deploy(WBTC, "Bitcoin", "WBTC");
+    await deployer.deploy(DAI, "DAI", "DAI");
+    await deployer.deploy(WETH, "Wrapper Ethereum", "WETH");
+    await deployer.deploy(SAND, "Sandbox", "SAND");
+    await deployer.deploy(USDC, "USDC", "USDC");
+    await deployer.deploy(USDT, "USDT", "USDT");
 };
